93088d3ee6787b5af30d93f4380e4c6ef3b69897
Fox <urboifox@gmail.com>
2025-07-26 15:28:01 +0300

    chore: remove mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -6,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
     // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
-    preprocess: [vitePreprocess(), mdsvex()],
+    preprocess: [vitePreprocess()],
     kit: {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
